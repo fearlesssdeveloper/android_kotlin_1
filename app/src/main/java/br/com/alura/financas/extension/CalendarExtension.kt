bf6eb71a35c25d62247d70cc1df85da909d36026
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-26 11:32:46 -0300

    android_kotlin_1 - 03 - Utilizando extension function

---
+++ app/src/main/java/br/com/alura/financas/extension/CalendarExtension.kt
@@ -0,0 +1,10 @@
+package br.com.alura.financas.extension
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Calendar.formataParaBrasileiro(): String {
+    val formatoBrasileiro = "dd/MM/yyyy"
+    val format = SimpleDateFormat(formatoBrasileiro)
+    return format.format(this.time)
+}
--- app/src/main/java/br/com/alura/financas/ui/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/br/com/alura/financas/ui/adapter/ListaTransacoesAdapter.kt
@@ -6,14 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import br.com.alura.financas.R
+import br.com.alura.financas.extension.formataParaBrasileiro
 import br.com.alura.financas.model.Transacao
 import kotlinx.android.synthetic.main.transacao_item.view.*
-import java.text.SimpleDateFormat
 
 class ListaTransacoesAdapter(
-    private val transacoes: List<Transacao>,
-    private val context: Context
-) : BaseAdapter(){
+        private val transacoes: List<Transacao>,
+        private val context: Context
+) : BaseAdapter() {
 
     override fun getCount(): Int {
         return transacoes.size
@@ -33,11 +33,10 @@ class ListaTransacoesAdapter(
 
         viewCriada.transacao_valor.text = transacao.valor.toString()
         viewCriada.transacao_categoria.text = transacao.categoria.toString()
-        val formatoBrasileiro = "dd/MM/yyyy"
-        val format = SimpleDateFormat(formatoBrasileiro)
-        val dataFormatada = format.format(transacao.data.time)
-        viewCriada.transacao_data.text = dataFormatada
+        viewCriada.transacao_data.text = transacao.data.formataParaBrasileiro()
 
         return viewCriada
     }
+
+
 }
