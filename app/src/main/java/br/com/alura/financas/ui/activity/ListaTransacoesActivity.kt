d12a68fb3b72c58dee0443515e1e5639b426f494
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-27 17:07:36 -0300

    android_kotlin_1 - 04 - Adicionando cores nas transações

--- app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
@@ -10,14 +10,16 @@ import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import java.math.BigDecimal
 import java.util.*
 
-class ListaTransacoesActivity : AppCompatActivity(){
+class ListaTransacoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
         val transacoes = listOf(
-            Transacao(tipo = Tipo.DESPESA, data = Calendar.getInstance(), valor = BigDecimal(20.5)),
-            Transacao(valor = BigDecimal(100.0), categoria = "Econômia", tipo = Tipo.RECEITA)
+                Transacao(tipo = Tipo.DESPESA, data = Calendar.getInstance(), valor = BigDecimal(20.5)),
+                Transacao(valor = BigDecimal(100.0), categoria = "Econômia", tipo = Tipo.RECEITA),
+                Transacao(valor = BigDecimal(200.0), tipo = Tipo.DESPESA),
+                Transacao(valor = BigDecimal(500.0), categoria = "Prêmio", tipo = Tipo.RECEITA)
         )
         lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
     }
--- app/src/main/java/br/com/alura/financas/ui/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/br/com/alura/financas/ui/adapter/ListaTransacoesAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.core.content.ContextCompat
 import br.com.alura.financas.R
 import br.com.alura.financas.extension.formataParaBrasileiro
+import br.com.alura.financas.model.Tipo
 import br.com.alura.financas.model.Transacao
 import kotlinx.android.synthetic.main.transacao_item.view.*
 
@@ -30,6 +32,18 @@ class ListaTransacoesAdapter(
     override fun getView(posicao: Int, view: View?, parent: ViewGroup?): View {
         val viewCriada = LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
         val transacao = transacoes[posicao]
+
+        if (transacao.tipo == Tipo.RECEITA) {
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(context, R.color.receita))
+        } else {
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(context, R.color.despesa))
+        }
+
+        if (transacao.tipo == Tipo.RECEITA) {
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_receita)
+        } else {
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_despesa)
+        }
 
         viewCriada.transacao_valor.text = transacao.valor.toString()
         viewCriada.transacao_categoria.text = transacao.categoria.toString()
