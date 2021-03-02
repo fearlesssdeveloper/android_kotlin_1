868589800347217045137921deb40ebee65c899d
Arlan Linux <marinhomoraisster@gmail.com>
2021-03-02 10:18:22 -0300

    android_kotlin_2 - 03 - Utilizando Single-Expression Function

--- app/src/main/java/br/com/alura/financas/model/Resumo.kt
+++ app/src/main/java/br/com/alura/financas/model/Resumo.kt
@@ -4,20 +4,18 @@ import java.math.BigDecimal
 
 class Resumo(private val transacoes: List<Transacao>) {
 
-    fun receita(): BigDecimal {
-        return transacoes.filter { transacao -> transacao.tipo == Tipo.RECEITA }
-                .sumByDouble { transacao -> transacao.valor.toDouble() }
-                .toBigDecimal()
-    }
+    fun receita(): BigDecimal = somaPor(Tipo.RECEITA)
+    
 
-    fun despesa(): BigDecimal {
-        return transacoes.filter { transacao -> transacao.tipo == Tipo.DESPESA }
-                .sumByDouble { transacao -> transacao.valor.toDouble() }
-                .toBigDecimal()
-    }
+    fun despesa(): BigDecimal = somaPor(Tipo.DESPESA)
 
-    fun total(): BigDecimal {
-        return receita().subtract(despesa())
-    }
+
+    fun total(): BigDecimal = receita().subtract(despesa())
+
+    private fun somaPor(tipo: Tipo): BigDecimal = transacoes.filter { it.tipo == tipo }
+                .sumByDouble { it.valor.toDouble() }
+                .toBigDecimal()
+
+    
 }
 
